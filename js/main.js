7ec0b597b068956fb37fe19f9aa5b724c2f2d4cd
Yuliia Voziian <vozianulia@gmail.com>
2024-08-23 00:00:49 +0300

    update swiper

--- js/main.js
+++ js/main.js
@@ -11,26 +11,89 @@ navigation: {
 // Булеты, текущее положение, прогрессбар
 pagination: {
     el: '.swiper-pagination',
-    // Булеты
-    type: 'bullets',
-    clickable: true,
-    // Динамические буллети
-    dynamicBullets: true,
-    // Кастомные буллеты
-    renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + '</span>';
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
     },
+    // Булеты
+    // type: 'bullets',
+    // clickable: true,
+    // // Динамические буллети
+    // dynamicBullets: true,
+    // // Кастомные буллеты
+    // renderBullet: function (index, className) {
+    //     return '<span class="' + className + '">' + (index + 1) + '</span>';
+    // },
     
     // Фракция
-    // type: 'fraction',
-    // // Кастомный вывод фракции
-    // renderFraction: function (currentClass, totalClass) {
-    //     return 'Foto <span class="' + currentClass + '"></span>' +
-    //     ' from' + '<span class="' + totalClass + '"></span>';
-    //  },
-
+    type: 'fraction',
+    // Кастомный вывод фракции
+    renderFraction: function (currentClass, totalClass) {
+        return 'Foto <span class="' + currentClass + '"></span>' +
+        ' from ' + '<span class="' + totalClass + '"></span>';
+     },
+    //  прогрессбар
     // type: 'progressbar',
     },
+    // скролбар
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        //возможность перетаскивать скролл
+        draggable: true
+    },
+
+    simulateTouch: true,
+    touchRadio: 1,
+    touchAngle: 45,
+    grabCursor: true,
+
+    slideToClickedSlide: true,
+
+    hashNavigation: {
+        watchState: true,
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+
+    },
+
+    mousewheel: {
+        sensitive: 1,
+    },
+
+    slidesPerView: 2,
+
+    spaceBetween: 10,
+
+    loop: true,
+
+    loopedSlides: 2,
+
+    // autoplay: {
+    //     delay: 1000,
+
+    //     stopOnLastSlide: true,
+
+    //     disableOnInteraction: false,
+    // }
+
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+        },
+        678: {
+            slidesPerView: 3,
+        },
+        712: {
+            slidesPerView: 7,
+        }
+    },
+
+
 
 });
 
